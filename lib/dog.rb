c05371b0c043aeb4fa3eed7664eff378724ac495
oneilld136 <oneilld136@gmail.com>
2019-04-24 12:52:09 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  def initialize(name, breed)
+      @name= name
+      @breed = breed
+    end
+
+    def name=(name)
+      @name = name
+
+    def breed=(breed)
+       if @breed = breed
+      else "Mutt"
+    end
+end
+end
+
+  end
+
+# name = Dog.new
+# name.breed = "Mutt"
